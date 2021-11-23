b237da3149eb2fb5d96358c749f2ceee7dee00b9
KRYMZ0N <jackgrimes05@yahoo.com>
2021-11-22 23:09:51 -0500

    Initial commit

---
+++ src/main/java/me/krymz0n/anarchyanticheat/Main.java
@@ -0,0 +1,23 @@
+package me.krymz0n.anarchyanticheat;
+
+import me.krymz0n.anarchyanticheat.movement.Fly;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        PluginManager pm = getServer().getPluginManager();
+
+        pm.registerEvents(this, this);
+        pm.registerEvents(new Fly(this), this);
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/me/krymz0n/anarchyanticheat/movement/Fly.java
@@ -0,0 +1,24 @@
+package me.krymz0n.anarchyanticheat.movement;
+
+import me.krymz0n.anarchyanticheat.Main;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class Fly implements Listener {
+    private final Main plugin;
+
+    public Fly(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    public void onPlayerMovement(PlayerMoveEvent evt) {
+        Player p = evt.getPlayer();
+
+        if (!p.isJumping()) {
+            if (p.isFlying()) {
+                evt.setCancelled(true);
+            }
+        }
+    }
+}
