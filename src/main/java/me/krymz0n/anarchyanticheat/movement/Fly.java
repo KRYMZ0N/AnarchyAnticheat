1233bbe7c9772afe5078a5d2580e5dc73ff7faf2
KRYMZ0N <jackgrimes05@yahoo.com>
2022-01-25 13:56:45 -0500

    Working on flight

--- src/main/java/me/krymz0n/anarchyanticheat/movement/Fly.java
+++ src/main/java/me/krymz0n/anarchyanticheat/movement/Fly.java
@@ -1,17 +1,19 @@
 package me.krymz0n.anarchyanticheat.movement;
 
 import me.krymz0n.anarchyanticheat.Main;
-import me.krymz0n.anarchyanticheat.utils.Utils;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
-import java.awt.*;
+import java.util.HashSet;
 
 public class Fly implements Listener {
     private final Main plugin;
+    HashSet<Player> check = new HashSet<>();
 
     public Fly(Main plugin) {
         this.plugin = plugin;
@@ -19,17 +21,29 @@ public class Fly implements Listener {
 
     @EventHandler
     public void onPlayerMovement(PlayerMoveEvent evt) {
-        Utils u = new Utils();
-        Player p = evt.getPlayer();
-        Location l = evt.getFrom();
+        Player  p = evt.getPlayer();
+        Location loc = p.getLocation();
+        Location newLoc = new Location(p.getWorld(), loc.getX(), loc.getY() - 1, loc.getZ());
+        double y = loc.getY() - 1; // Checking block underneath
 
-        if (p.isFlying()) {
-            evt.setCancelled(true);
-            evt.setTo(evt.getFrom());
+        // --since this event gets called by movement, we add the player to the set, and when called again we remove if false--
+        if (newLoc.getBlock().getType() == Material.AIR) {
+            check.add(p); // add if in air
+        } else {
+            if (check.contains(p)) {
+                check.remove(p); // remove if not in the set on the next calls.
+            }
         }
-        if (u.isInAir(evt.getPlayer())) {
-            evt.setCancelled(true);
-            evt.setTo(l.subtract( 0, 1, 0));
+
+        if (check.contains(p)) {
+            new BukkitRunnable() {
+                public void run() { // running a task with a delay to try and prevent player from ascending.
+                    if (loc.getY() > y) { // if player ascends cancel
+                        evt.setCancelled(true);
+
+                    }
+                }
+            }.runTaskLaterAsynchronously(plugin, 20L);
         }
     }
 }
--- src/main/java/me/krymz0n/anarchyanticheat/utils/Utils.java
+++ src/main/java/me/krymz0n/anarchyanticheat/utils/Utils.java
@@ -6,27 +6,11 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 public class Utils {
-    BoolReference bf = new BoolReference(false);
-
-    public boolean isInAir(Player p) {
-        Location l = p.getLocation();
-
-        if (l.getBlock().getRelative(BlockFace.DOWN).getType().equals(Material.AIR)) { // gets the block under player
-
-            Bukkit.getScheduler().runTaskLater(Main.instance, () -> {
-                if (l.getBlock().getRelative(BlockFace.DOWN).getType().equals(Material.AIR)) {
-                    bf.set(true);
-                }
-                else bf.set(false);
-            }, 20L); // amount to wait in ticks , 20 ticks = 1 second
-
-            while (!bf.hasBeenEdited()) {
-                p.sendMessage(bf.get().toString());
-                return bf.get();
-            }
-        }
-        return false;
-    }
 }
