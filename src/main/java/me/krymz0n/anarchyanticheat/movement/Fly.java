1159395f7748ff5aace25edb1db6c71387153930
KRYMZ0N <jackgrimes05@yahoo.com>
2022-01-25 22:04:20 -0500

    trying to get it to work lmfao

--- src/main/java/me/krymz0n/anarchyanticheat/movement/Fly.java
+++ src/main/java/me/krymz0n/anarchyanticheat/movement/Fly.java
@@ -21,29 +21,43 @@ public class Fly implements Listener {
 
     @EventHandler
     public void onPlayerMovement(PlayerMoveEvent evt) {
+        System.out.println("Event Called");
         Player  p = evt.getPlayer();
         Location loc = p.getLocation();
+        final Location prevLoc = loc;
         Location newLoc = new Location(p.getWorld(), loc.getX(), loc.getY() - 1, loc.getZ());
-        double y = loc.getY() - 1; // Checking block underneath
+        final double y = loc.getY() - 1; // Checking block underneath
 
         // --since this event gets called by movement, we add the player to the set, and when called again we remove if false--
         if (newLoc.getBlock().getType() == Material.AIR) {
+            System.out.println("added");
             check.add(p); // add if in air
         } else {
             if (check.contains(p)) {
+                System.out.println("removed");
                 check.remove(p); // remove if not in the set on the next calls.
             }
         }
 
         if (check.contains(p)) {
+            System.out.println("contains");
             new BukkitRunnable() {
                 public void run() { // running a task with a delay to try and prevent player from ascending.
-                    if (loc.getY() > y) { // if player ascends cancel
-                        evt.setCancelled(true);
+                    System.out.println("runnable");
+                    if (loc.getY() - y >= 1.0d && !p.isJumping()) { // if player ascends cancel
+                        System.out.println("should be cancelled.");
+                        for(int y = prevLoc.getBlockY(); y > 0; y--) { // get current Y coordinate, go down until we hit bedrock (0)
+                            if(prevLoc.subtract(0, 1, 0).getBlock().getType() == Material.AIR) continue; // if the block is air, go to next
+                            p.teleport(prevLoc.add(0, 1, 0)); // teleport player to the block above, otherwise he would be stuck in the current one
+                            break; // stop the loop
+                        }
+                        check.remove(p);
 
+                    } else {
+                        check.remove(p);
                     }
                 }
-            }.runTaskLaterAsynchronously(plugin, 20L);
+            }.runTaskLater(plugin, 20L);
         }
     }
 }
